c73ec7dc58fb3b8d614e239bcd53ef7f04467b9d
Juan <dzn50346@educastur.es>
2025-11-26 15:20:22 +0100

    afalta parte de la creaci´n de una asignatura_

--- src/main/java/com/dzn50346/gestionDeDocentes/services/AsignaturaService.java
+++ src/main/java/com/dzn50346/gestionDeDocentes/services/AsignaturaService.java
@@ -2,7 +2,9 @@ package com.dzn50346.gestionDeDocentes.services;
 
 import com.dzn50346.gestionDeDocentes.dto.AsignaturaDTO;
 import com.dzn50346.gestionDeDocentes.models.Asignatura;
+import com.dzn50346.gestionDeDocentes.models.Ciclo;
 import com.dzn50346.gestionDeDocentes.repositories.AsignaturaRepository;
+import com.dzn50346.gestionDeDocentes.repositories.CicloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class AsignaturaService {
 
     private final AsignaturaRepository repository;
+    private CicloRepository cicloRepository;
 
     @Autowired
     public AsignaturaService(AsignaturaRepository repository) {
@@ -25,8 +28,11 @@ public class AsignaturaService {
             throw new IllegalArgumentException("Ya existe una asignatura con el id: " + asignaturaDTO.getIdDTO());
         }
 
+        Ciclo ciclo = cicloRepository.findById(asignaturaDTO.getIdCicloDTO()).orElseThrow(() -> new RuntimeException("Ciclo no encontrado con id: " + asignaturaDTO.getIdCicloDTO()));
+
+
         Asignatura asignatura = new Asignatura();
         asignatura.setNombre(asignaturaDTO.getNombreDTO());
-        asignatura.setCiclo();
+        asignatura.setCiclo(ciclo);
     }
 }
